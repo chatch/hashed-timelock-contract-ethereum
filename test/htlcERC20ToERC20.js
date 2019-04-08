bdc81f88d2967efe6f0e605167d14f6e3f77c8a5
Jim Zhang <jim.zhang@consensys.net>
2019-04-07 22:46:13 -0400

    Added tests for refund scenarios

--- test/htlcERC20ToERC20.js
+++ test/htlcERC20ToERC20.js
@@ -9,14 +9,16 @@ const {
   txContractId,
   txLoggedArgs,
 } = require('./helper/utils');
+const promisify = require('util').promisify;
+const sleep = promisify(require('timers').setTimeout);
+const truffleAssert = require('truffle-assertions');
 
 const HashedTimelockERC20 = artifacts.require('./HashedTimelockERC20.sol')
 const ASEANTokenContract = artifacts.require('./helper/ASEANToken.sol')
 const EUTokenContract = artifacts.require('./helper/EUToken.sol')
 
 // some testing data
-const hourSeconds = 3600
-const timeLock1Hour = nowSeconds() + hourSeconds
+let timeLock2Sec
 const tokenAmount = 5
 
 contract('HashedTimelock swap between two ERC20 tokens', accounts => {
@@ -82,7 +84,8 @@ contract('HashedTimelock swap between two ERC20 tokens', accounts => {
   // without fulfilling his side of the deal, because this transfer is locked by a hashed secret
   // that only Alice knows at this point
   it('Step 1: Alice sets up a swap with Bob in the ASEANToken contract', async () => {
-    const newSwapTx = await newSwap(ASEANToken, htlc, {hashlock: hashPair.hash, timelock: timeLock1Hour}, Alice, Bob)
+    timeLock2Sec = nowSeconds() + 2
+    const newSwapTx = await newSwap(ASEANToken, htlc, {hashlock: hashPair.hash, timelock: timeLock2Sec}, Alice, Bob)
     a2bSwapId = txContractId(newSwapTx)
 
     // check token balances
@@ -98,7 +101,8 @@ contract('HashedTimelock swap between two ERC20 tokens', accounts => {
   it('Step 2: Bob sets up a swap with Alice in the EUToken contract', async () => {
     // in a real world swap contract, the counterparty's swap timeout period should be shorter
     // but that does not affect the ideal workflow that we are testing here
-    const newSwapTx = await newSwap(EUToken, htlc, {hashlock: hashPair.hash, timelock: timeLock1Hour}, Bob, Alice)
+    timeLock2Sec = nowSeconds() + 2
+    const newSwapTx = await newSwap(EUToken, htlc, {hashlock: hashPair.hash, timelock: timeLock2Sec}, Bob, Alice)
     b2aSwapId = txContractId(newSwapTx)
 
     // check token balances
@@ -157,6 +161,49 @@ contract('HashedTimelock swap between two ERC20 tokens', accounts => {
       msg ? msg : 'wrong token balance'
     )
   }
+
+  describe("Test the refund scenario:", () => {
+    const currentBalanceAlice = senderInitialBalance - tokenAmount;
+    const currentBalanceBob = senderInitialBalance - tokenAmount;
+
+    it('the swap is set up with 5sec timeout on both sides', async () => {
+      timeLock2Sec = nowSeconds() + 2
+      let newSwapTx = await newSwap(ASEANToken, htlc, {hashlock: hashPair.hash, timelock: timeLock2Sec}, Alice, Bob)
+      a2bSwapId = txContractId(newSwapTx);
+
+      newSwapTx = await newSwap(EUToken, htlc, {hashlock: hashPair.hash, timelock: timeLock2Sec}, Bob, Alice)
+      b2aSwapId = txContractId(newSwapTx)
+
+      assertTokenBal(ASEANToken, htlc.address, tokenAmount);
+      assertTokenBal(EUToken, htlc.address, tokenAmount);
+
+      await sleep(2000);
+
+      // after the timeout expiry Alice calls refund() to get her tokens back
+      let result = await htlc.refund(a2bSwapId, {
+        from: Alice
+      });
+
+      // verify the event was emitted
+      truffleAssert.eventEmitted(result, 'HTLCERC20Refund', ev => {
+        return ev.contractId === a2bSwapId;
+      }, `Refunded Alice`);
+
+      await assertTokenBal(ASEANToken, Alice, currentBalanceAlice);
+
+      // Bob can also get his tokens back by calling refund()
+      result = await htlc.refund(b2aSwapId, {
+        from: Bob
+      });
+
+      // verify the event was emitted
+      truffleAssert.eventEmitted(result, 'HTLCERC20Refund', ev => {
+        return ev.contractId === b2aSwapId;
+      }, `Refunded Bob`);
+
+      assertTokenBal(EUToken, Bob, currentBalanceBob);
+    });
+  })
 
   const newSwap = async (token, htlc, config, initiator, counterparty) => {
     // initiator of the swap has to first designate the swap contract as a spender of his/her money
