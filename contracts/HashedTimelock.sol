223155945dc238eb7c726ea7d3fdc208aa24a268
icodezjb <icodezjb@163.com>
2019-04-22 10:17:53 +0800

    fix typo

--- contracts/HashedTimelock.sol
+++ contracts/HashedTimelock.sol
@@ -15,7 +15,7 @@ pragma solidity ^0.5.0;
  *  2) withdraw(contractId, preimage) - once the receiver knows the preimage of
  *      the hashlock hash they can claim the ETH with this function
  *  3) refund() - after timelock has expired and if the receiver did not 
- *      withdraw funds the sender / creater of the HTLC can get their ETH 
+ *      withdraw funds the sender / creator of the HTLC can get their ETH 
  *      back with this function.
  */
 contract HashedTimelock {
